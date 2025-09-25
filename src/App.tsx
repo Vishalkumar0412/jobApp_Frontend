9051d1e09c22c660e3ff3f4dcf6dc8c1932c6d26
vishal kumar <iamvishalnigam@gmail.com>
2025-09-25 16:12:49 +0530

    initail commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div 
+      style={{
+        fontSize: "24px",
+        textAlign: "center",
+        marginTop: "20%",
+        padding: "0 20px",
+        lineHeight: "1.5",
+      }}
+    >
+      🚧 The frontend of Jobify is currently under construction. <br />
+      You can check out the backend progress here:{" "}
+      <a 
+        href="https://github.com/Vishalkumar0412/jobApp_backend.git" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        style={{ color: "#0070f3", textDecoration: "underline" }}
+      >
+        Jobify Backend GitHub
+      </a>
+    </div>
+  );
+};
+
+export default App;
